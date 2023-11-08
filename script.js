fe7c6010611226bc7cd2094c9a33dd0534df04b0
Maxim Latnikov <latnikou@gmail.com>
2023-11-08 12:54:30 +0300

    добавил задание

---
+++ script.js
@@ -0,0 +1,24 @@
+// задача №1
+const fill = (arraySize, value) => {
+  // решение здесь
+}
+
+const data = 3;
+const valueToFill = 'a';
+console.log(fill(data, valueToFill))
+
+// задача №2
+const reverse = (array) => {
+  // решение здесь
+ }
+
+const data = [1, 2, 3];
+console.log(reverse(data)); // [3, 2, 1]
+
+// задача №3
+const compact = (array) => {
+  // решение здесь
+}
+
+const data = [0, 1, false, 2, undefined, '', 3, null];
+console.log(compact(data)) // [1, 2, 3]
